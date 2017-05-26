955ecba8fdae75d4c8378d7da631a586fa79ae97
youseflkc <youseflkc@yahoo.ca>
2017-05-25 22:19:58 -0400

    fix

--- 8 Ball Pool/src/Main.java
+++ 8 Ball Pool/src/Main.java
@@ -6,6 +6,8 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
@@ -21,9 +23,10 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Main {
-		
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -49,11 +52,11 @@ public class Main {
 		splashScreen.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		JFrame mainScreen = new JFrame();
 		mainScreen.setSize(1366, 768);
-		
+
 		mainScreen.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		mainScreen.setUndecorated(true);
 		mainScreen.setVisible(false);
-		
+
 		try {
 			splashScreen.setContentPane(
 					new JLabel(new ImageIcon(ImageIO.read(new File("Pictures/8 Ball Pool SplashScreen.jpg")))));
@@ -63,7 +66,7 @@ public class Main {
 		splashScreen.getContentPane().setLayout(new BorderLayout(5, 5));
 		splashScreen.setUndecorated(true);
 		splashScreen.setVisible(true);
-				
+
 		JLabel anyKeyLabel = new JLabel("PRESS ANY KEY TO CONTINUE...");
 		anyKeyLabel.setFont(new Font("Roboto", Font.PLAIN, 40));
 		anyKeyLabel.setForeground(Color.white);
@@ -92,27 +95,6 @@ public class Main {
 		mainScreen.getContentPane().setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 
-		JLabel pressExit = new JLabel("Press 'e' to Exit");
-		pressExit.setFont(new Font("Impact", Font.PLAIN, 22));
-		pressExit.setForeground(Color.WHITE);
-
-		mainScreen.getContentPane().requestFocusInWindow();
-		mainScreen.addKeyListener(new KeyListener() {
-			public void keyPressed(KeyEvent e) {
-				if (e.getKeyChar() == 'e') {
-					if (JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Exit",
-							JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-						System.exit(0);
-					}
-				}
-			}
-
-			public void keyTyped(KeyEvent e) {
-			}
-
-			public void keyReleased(KeyEvent e) {
-			}
-		});
 
 		JPanel menuPane = new JPanel();
 		menuPane.setBackground(new Color(0, 0, 0, 90));
@@ -123,7 +105,7 @@ public class Main {
 		JPanel helpPane = new JPanel();
 		helpPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		helpPane.setLayout(new BoxLayout(helpPane, BoxLayout.Y_AXIS));
-		helpPane.setBackground(new Color(0, 0, 0, 0));
+		helpPane.setBackground(new Color(0, 0, 0, 25));
 		helpPane.setPreferredSize(new Dimension(400, 600));
 
 		c.gridx = 0;
@@ -134,26 +116,55 @@ public class Main {
 		c.gridx = 1;
 		mainScreen.getContentPane().add(helpPane, c);
 
-		pressExit.setAlignmentX(helpPane.RIGHT_ALIGNMENT);
-		helpPane.add(pressExit);
-
 		JButton playButton = button("Play");
 		playButton.setAlignmentX(menuPane.CENTER_ALIGNMENT);
 		menuPane.add(playButton);
-	
-		menuPane.add(Box.createRigidArea(new Dimension(0,75)));
-		
-		JButton helpButton= button ("Help");
+
+		menuPane.add(Box.createRigidArea(new Dimension(0, 75)));
+
+		JButton helpButton = button("Help");
 		helpButton.setAlignmentX(menuPane.CENTER_ALIGNMENT);
 		menuPane.add(helpButton);
-		
-		menuPane.add(Box.createRigidArea(new Dimension(0,75)));
 
-		JButton settingsButton= button("Settings");
+		menuPane.add(Box.createRigidArea(new Dimension(0, 75)));
+
+		JButton settingsButton = button("Settings");
 		settingsButton.setAlignmentX(menuPane.CENTER_ALIGNMENT);
 		menuPane.add(settingsButton);
+		
+		menuPane.add(Box.createRigidArea(new Dimension(0, 75)));
+		
+		JButton exitButton =button("Exit");
+		exitButton.setAlignmentX(menuPane.CENTER_ALIGNMENT);
+		menuPane.add(exitButton);
+
+		helpButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JTextArea helpText = new JTextArea();
+				helpText.setText("How to Play 8 Ball Pool\n\n There are seven solid-colored balls numbered 1 through 7,"
+						+ "\nseven striped balls numbered 9 through 15, an 8 ball, and a cue ball. One of the players is "
+						+ "\nrandomly chosen to break (hit the cue ball against the setup of balls) at the start.\nA player (or team)"
+						+ "\nwill continue to shoot until committing a foul"
+						+ ",\nor failing to legally pocket an object ball (whether intentionally or not). Thereupon it is the turn of the opposing "
+						+ "\nplayer(s). Play alternates in this manner for the remainder of the game. Following a foul, the incoming"
+						+ "\nplayer has ball-in-hand anywhere on the table, unless the foul occurred on the break shot, as noted previously.");
+				
+				helpText.setEditable(false);
+				helpText.setFont(new Font("Roboto",Font.PLAIN,16));
+				helpText.setForeground(Color.WHITE);
+				helpText.setBackground(Color.BLACK);
+				helpPane.add(helpText);
+				mainScreen.revalidate();
+			}	
+		});
+		
+		exitButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				if(JOptionPane.showConfirmDialog(null,"Are you sure you want to exit?","Exit",JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION){
+					System.exit(0);
+				}
+			}
+		});
 	}
-	
-	
 
 }
