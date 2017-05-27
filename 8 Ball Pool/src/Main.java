fb3fea321bf3105d76ad55ad931e072763f321e7
youseflkc <youseflkc@yahoo.ca>
2017-05-26 21:44:24 -0400

    added music method

--- 8 Ball Pool/src/Main.java
+++ 8 Ball Pool/src/Main.java
@@ -15,6 +15,10 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -26,15 +30,33 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+
 public class Main {
-	
+
 	private Overlay overlay;
 	private Billiard content;
-	
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();//Grabs screen resolution and fixes the size of the JFrame
+	String helpString = "\t        How to Play 8 Ball Pool\n\n -There are 7 solid, and 7 striped balls, "
+			+ "a black 8-ball, and a white cue-ball \n The first player to sink a ball gets to play for the ball he sunk "
+			+ "ie. if player 1 sinks a striped ball first, then player 1 is stripes, and player 2 is solids"
+			+ " \n -A player is randomly chosen to break\n -If a ball is sunk, the player keeps playing until they miss "
+			+ "\n -Once they miss, it's the next player's turn \n -Sink all of the designated balls, and then shoot"
+			+ " at the 8-ball last to win \n\n -The 8-ball must be sunk last—sinking it before then will result"
+			+ " in an automatic loss \n -If the cue ball is sunk, the next player gets their turn with the ball in hand \n"
+			+ " -The cue ball must touch that player's type of ball (striped or solid), and the coloured ball that was hit"
+			+ " or the cue ball must touch a side of the table";
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();// Grabs
+																		// screen
+																		// resolution
+																		// and
+																		// fixes
+																		// the
+																		// size
+																		// of
+																		// the
+																		// JFrame
 	double width = screenSize.getWidth();
 	double height = screenSize.getHeight();
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -49,7 +71,7 @@ public class Main {
 		newButton.setFocusPainted(false);
 		newButton.setFont(new Font("Magneto", Font.BOLD, 26));
 		newButton.setFocusable(false);
-		newButton.setBackground(new Color(30,175,30));
+		newButton.setBackground(new Color(30, 175, 30));
 		newButton.setMaximumSize(new Dimension(225, 50));
 		return newButton;
 	}
@@ -66,8 +88,8 @@ public class Main {
 		mainScreen.setVisible(false);
 
 		try {
-			splashScreen.setContentPane(
-					new JLabel(new ImageIcon(ImageIO.read(new File("8 Ball Pool/resource/Images/8 Ball Pool SplashScreen.jpg")))));
+			splashScreen.setContentPane(new JLabel(
+					new ImageIcon(ImageIO.read(new File("8 Ball Pool/resource/Images/8 Ball Pool SplashScreen.jpg")))));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -80,11 +102,20 @@ public class Main {
 		anyKeyLabel.setFont(new Font("Impact", Font.PLAIN, 40));
 		splashScreen.getContentPane().add(anyKeyLabel, BorderLayout.PAGE_END);
 		anyKeyLabel.startBlinking();
-		
+
 		splashScreen.addKeyListener(new KeyListener() {
 			public void keyPressed(KeyEvent e) {
 				splashScreen.dispose();
 				mainScreen.setVisible(true);
+				try {
+					playMusic();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (Throwable e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 
 			public void keyReleased(KeyEvent e) {
@@ -95,16 +126,16 @@ public class Main {
 		});
 
 		try {
-			mainScreen.setContentPane(
-					new JLabel(new ImageIcon(ImageIO.read(new File("8 Ball Pool/resource/Images/main menu background.jpg")))));
+			mainScreen.setContentPane(new JLabel(
+					new ImageIcon(ImageIO.read(new File("8 Ball Pool/resource/Images/main menu background.jpg")))));
 		} catch (IOException e) {
-			e.printStackTrace();     //Commented out the exception because it was annoying seeing it every 
-									// time i go to the next screen 
+			e.printStackTrace(); // Commented out the exception because it was
+									// annoying seeing it every
+									// time i go to the next screen
 		}
 
 		mainScreen.getContentPane().setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-
 
 		JPanel menuPane = new JPanel();
 		menuPane.setBackground(new Color(0, 0, 0, 100));
@@ -130,20 +161,17 @@ public class Main {
 		playButton.setAlignmentX(menuPane.CENTER_ALIGNMENT);
 		menuPane.add(Box.createRigidArea(new Dimension(0, 50)));
 		menuPane.add(playButton);
-		
+
 		playButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				content = new Billiard ();
-				mainScreen.setContentPane (content);
-				
-				
-				overlay = new Overlay ();
-				mainScreen.setGlassPane (overlay);
-				mainScreen.getGlassPane ().setVisible (true);
-			}	
+				content = new Billiard();
+				mainScreen.setContentPane(content);
+
+				overlay = new Overlay();
+				mainScreen.setGlassPane(overlay);
+				mainScreen.getGlassPane().setVisible(true);
+			}
 		});
-		
-		
 
 		menuPane.add(Box.createRigidArea(new Dimension(0, 75)));
 
@@ -156,10 +184,10 @@ public class Main {
 		JButton settingsButton = button("Settings");
 		settingsButton.setAlignmentX(menuPane.CENTER_ALIGNMENT);
 		menuPane.add(settingsButton);
-		
+
 		menuPane.add(Box.createRigidArea(new Dimension(0, 75)));
-		
-		JButton exitButton =button("Exit");
+
+		JButton exitButton = button("Exit");
 		exitButton.setAlignmentX(menuPane.CENTER_ALIGNMENT);
 		menuPane.add(exitButton);
 
@@ -168,35 +196,42 @@ public class Main {
 				helpPane.setBackground(new Color(0, 0, 0, 150));
 				helpPane.setVisible(true);
 				JTextArea helpText = new JTextArea();
-				helpText.setText("\t        How to Play 8 Ball Pool\n\n -There are 7 solid, and 7 striped balls, "
-						+ "a black 8-ball, and a white cue-ball \n The first player to sink a ball gets to play for the ball he sunk "
-						+ "ie. if player 1 sinks a striped ball first, then player 1 is stripes, and player 2 is solids"
-						+ " \n -A player is randomly chosen to break\n -If a ball is sunk, the player keeps playing until they miss "
-						+ "\n -Once they miss, it's the next player's turn \n -Sink all of the designated balls, and then shoot"
-						+ " at the 8-ball last to win \n\n -The 8-ball must be sunk last—sinking it before then will result"
-						+ " in an automatic loss \n -If the cue ball is sunk, the next player gets their turn with the ball in hand \n"
-						+ " -The cue ball must touch that player's type of ball (striped or solid), and the coloured ball that was hit"
-						+ " or the cue ball must touch a side of the table");
-				
+				helpText.setText(helpString);
 				helpText.setEditable(false);
-				helpText.setFont(new Font("High Tower Text",Font.PLAIN,20));
+				helpText.setFont(new Font("High Tower Text", Font.PLAIN, 20));
 				helpText.setForeground(Color.WHITE);
-				helpText.setBackground(new Color(0,0,0,0));
+				helpText.setBackground(new Color(0, 0, 0, 0));
 				helpText.setLineWrap(true);
 				helpText.setWrapStyleWord(true);
 				helpPane.add(helpText);
+
 				mainScreen.revalidate();
 				mainScreen.repaint();
-			}	
+			}
 		});
-		
-		exitButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				if(JOptionPane.showConfirmDialog(null,"Are you sure you want to exit?","Exit",JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION){
+
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Exit",
+						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
 					System.exit(0);
 				}
 			}
 		});
+	}
+	
+	/**
+	 * Plays music in the background
+	 * @throws Throwable
+	 * @throws IOException
+	 */
+	public void playMusic() throws Throwable, IOException {
+		 Clip clip = AudioSystem.getClip();
+	        AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("8 Ball Pool/resource/Music/This City Prod. David Wud.wav"));
+	        clip.open(inputStream);
+	        FloatControl volume=(FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+	        volume.setValue(-10); //increase or decrease volume in decibals
+	        clip.start(); 
 	}
 
 }
